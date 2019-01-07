ce8bacf664e1b4a70556fb23f44900cab01a8f56
drinith <felpmel@gmail.com>
2019-01-07 00:21:25 -0200

    primeiro

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,15 @@
+//Ele acessa esses caras através do local das dependecias
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import Routes from './routes'
+import Menu from '../menu/menu'
+
+export default props =>(
+    <div className='container'>
+        <Menu/>
+        <Routes />
+    </div>
+
+)
